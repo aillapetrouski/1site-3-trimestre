5135c591339433aac7f6ecf55fae2f51e48ca103
aillapetrouski <160617731+aillapetrouski@users.noreply.github.com>
2024-10-24 11:25:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Joias',
+    'Qual a diferença entre ouro 18k e 24k?',
+    'O Python é uma linguagem de programação O ouro 18k é uma liga composta por 75% de ouro puro e 25% de outros metais, como cobre ou prata, o que o torna mais resistente e durável. Já o ouro 24k é ouro puro.'
+)
+
+criaCartao(
+    'Joias',
+    'Como cuidar de joias de prata para evitar que escureçam?',
+    'A capital da França é ParisPara conservar suas joias de prata, evite o contato com produtos químicos como perfumes, cremes e água do mar. Limpe-as regularmente com um pano macio e específico para prata. Guarde-as em locais secos e escuros, preferencialmente em embalagens individuais.'
+)
+
+criaCartao(
+    'Joias',
+    'O que significa a marca 925 em uma joia de prata?',
+    'Uma função é um bloco de código que executa alguma tarefaA marca 925 indica que a joia é feita de prata de lei, ou seja, contém 92,5% de prata pura e 7,5% de outros metais, como cobre. Essa liga garante a durabilidade e beleza da peça.'
+)
+
+criaCartao(
+    'Joias',
+    'Qual a melhor pedra para um anel de noivado?',
+    'Oi em ingles é HI (RAI)A escolha da pedra para um anel de noivado é muito pessoal e depende dos gostos e do orçamento do casal. Diamantes são os mais tradicionais, mas outras opções como safiras, rubis e esmeraldas também são muito populares e significativas.'
+)
+
+criaCartao(
+    'Joias',
+    'Como escolher o tamanho certo de um anel?',
+    'Oi em ingles é HI (RAI) A forma mais precisa de descobrir o tamanho do seu anel é medindo o dedo com um anel medidor ou utilizando um paquímetro. Caso não tenha essas ferramentas, você pode medir o diâmetro interno de um anel que já sirva e comparar com as tabelas de medidas disponíveis nas joalherias.'
+)
+
+criaCartao(
+    'Joias',
+    'Qual a diferença entre joias e bijuterias?',
+    'Oi em ingles é HI (RAI)A principal diferença entre joias e bijuterias está nos materiais utilizados. Joias são feitas de metais preciosos como ouro, prata e platina, e pedras preciosas ou semipreciosas. Bijuterias, por sua vez, são feitas de metais não nobres, como latão ou aço inoxidável, e pedras sintéticas ou cristais. Joias geralmente possuem maior durabilidade e valor.'
+)
